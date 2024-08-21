8b3b768baa876dd50bfb026ed3e25bf925faa084
SEAMUSJP <harubaru0414@gmail.com>
2024-08-21 23:41:14 +0900

    Initial Commit

---
+++ EmbeddedCalc/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EmbeddedCalc
+//
+//  Created by 少路春希 on 2024/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmbeddedCalc/EmbeddedCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmbeddedCalcApp.swift
+//  EmbeddedCalc
+//
+//  Created by 少路春希 on 2024/08/21.
+//
+
+import SwiftUI
+
+@main
+struct EmbeddedCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
